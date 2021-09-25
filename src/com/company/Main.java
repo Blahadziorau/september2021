3ff90458c05a15fa4998dbd21fb0fe895d3a6362
blagothestars <rollerxander@gmail.com>
2021-09-26 00:27:21 +0200

    test commit by gitbash

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
        // BazaRolikov.para();
         System.out.println(BazaRolikov.para());
+        System.out.println("thank you");
     }
 }
