c9c62293cf3b3eb8a9740e3505f7d7c27f0744ec
Blahadziorau <rollerxander@gmail.com>
2021-11-28 00:15:04 +0100

    Class ArrScan + loop For Each

--- Codility/src/Random/ArrScan.java
+++ Codility/src/Random/ArrScan.java
@@ -15,27 +15,24 @@ public class ArrScan {
         Scanner scn1 = new Scanner(file);
         while (scn.hasNext()) {
             ss.add(scn.next());              //every element of raw to Arraylist
-
         }
-
         scn.close();
-
         System.out.println("ss  ---> " + ss);
-
 /*
         System.out.println(ss.get(0));
         System.out.println(ss.get(1));
         System.out.println(ss.get(2));
 */
-
         while (scn1.hasNext()) {
             ss2.add(scn1.nextLine());            //every raw to Arraylist
-
         }
-
-
         scn1.close();
         System.out.println("ss2 ---> " + ss2);
-
+        for (Object e: ss2             ) {
+            System.out.println(e);
+        }
+        for (Object e: ss             ) {
+            System.out.println(e);
+        }
     }
 }
